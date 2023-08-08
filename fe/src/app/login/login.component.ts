20b64d5c55c8367465b2e792ab7258efa7303041
draibinadnan@gmail.com <draibinadnan@gmail.com>
2023-08-08 23:36:49 +0200

    fix login

--- fe/src/app/login/login.component.ts
+++ fe/src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { NotifierService } from 'angular-notifier';
+import { RipristinaCredenzialiComponent } from '../ripristina-credenziali/ripristina-credenziali.component';
 
 @Component({
   selector: 'app-login',
@@ -15,22 +16,25 @@ export class LoginComponent implements Stage{
   public loginForm:FormGroup;
   @Output() changeType: EventEmitter<any>= new EventEmitter<any>();
   
-  constructor(private notifier: NotifierService,private fb:FormBuilder,private http:HttpClient,private dashboard:DashboardComponent){
+  constructor(private notifier: NotifierService,private fb:FormBuilder,private http:HttpClient,private dashboard:DashboardComponent,private ripristinaCred:RipristinaCredenzialiComponent){
     this.loginForm =  this.fb.group({
       identificativo: [null,Validators.required],
-      password: [null,[Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]]
+      password: [null,Validators.required]
     });
+    //Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}'
   }
 
   login(){
-    //if(!this.loginForm.valid)
-      //return;    
-      this.notifier.notify("success", "ciao" );
-    //this.http.post("login.json",this.loginForm.value).subscribe((res)=>{
-      //if(res)
-        //this.renew(this.dashboard);
-       // this.changeType.emit(DashboardComponent);
-    //})
+    if(!this.loginForm.valid)
+      return;    
+
+    this.http.post("/api/login.json",this.loginForm.value).subscribe((res:any)=>{
+      if(res){
+        sessionStorage.setItem("token",res.token);
+        this.renew(this.dashboard);
+        this.changeType.emit(DashboardComponent);
+      }
+    })
   }
 
   renew(newType: Stage): void {
@@ -38,7 +42,8 @@ export class LoginComponent implements Stage{
   }
 
   ripristinaPassword(){
-
+    this.renew(this.ripristinaCred);
+    this.changeType.emit(RipristinaCredenzialiComponent);
   }
 
   creaAccount(){
