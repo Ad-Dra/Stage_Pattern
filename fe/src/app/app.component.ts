f36e1517dc6252ff7802fc451927a216bf0ede34
draibinadnan@gmail.com <draibinadnan@gmail.com>
2023-08-09 15:37:22 +0200

    fix navBar appComponent

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -11,15 +11,24 @@ import { ConfermaCreazioneAccountComponent } from './conferma-creazione-account/
 export class AppComponent implements OnInit{
   title = 'Stage_Pattern';
   public type:any=LoginComponent;
+  public showNavBar:boolean=false;
 
   ngOnInit(): void {
     if(location.hash.includes('#/ripristinaPassword'))
       this.type=RipristinaCredenzialiComponent;
     else if(location.hash.includes('#/confermaEmail'))
       this.type=ConfermaCreazioneAccountComponent;
+
+    this.showOrHideNavBar();
+  }
+
+  showOrHideNavBar(){
+    if(this.type.name!="LoginComponent" && this.type.name!="RipristinaCredenzialiComponent" && this.type.name!="CreaUtenzaComponent" && this.type.name!="ConfermaCreazioneAccountComponent")
+      this.showNavBar=true;
   }
 
   refreshTypeObject(typeObject:any){
     this.type=typeObject;
+    this.showOrHideNavBar();
   }
 }
