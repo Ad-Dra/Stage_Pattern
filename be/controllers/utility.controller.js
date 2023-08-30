879a2fc57f1c063ca2efb00d2f274664ae380965
draibinadnan@gmail.com <draibinadnan@gmail.com>
2023-08-30 21:29:39 +0200

    fix description log on utility service

--- be/controllers/utility.controller.js
+++ be/controllers/utility.controller.js
@@ -103,8 +103,7 @@ exports.isActiveUser=(dati,email)=>{
       let transporter= nodeMailer.createTransport({
         host:"smtp.gmail.com",
         port: 587,
-        secure:false,
-        
+        secure:false
       })
       
   
@@ -172,13 +171,13 @@ exports.getDescriptionForEvolution=async (username,state)=>{
 
   switch(state){
     case 0:
-      return username+": evoluzione in dashboard senza conto corrente \n                                    Le operazioni permesse sono: 1) Consultare chi siamo 2) Info account 3) Log out";
+      return username+": si è evoluto in UtenteNuovo \n                                    Le operazioni permesse sono: 1) Consultare chi siamo 2) Info account 3) Log out";
     case 1:
-      return username+": evoluzione in dashboard con saldo attivo \n                                    Le operazioni permesse sono: 1) Consultare chi siamo 2) Effettuare bonifici/ricariche telefoniche 3) Richiedere prestito 4) Info account 5) Consultare i movimenti 6) Log out";
+      return username+": si è evoluto in UtenteSaldoAttivo \n                                    Le operazioni permesse sono: 1) Consultare chi siamo 2) Effettuare bonifici/ricariche telefoniche 3) Richiedere prestito 4) Info account 5) Consultare i movimenti 6) Log out";
     case 2:
-      return username+": evoluzione in dashboard con saldo passivo \n                                    Le operazioni permesse sono: 1) Consultare chi siamo 2) Richiedere prestito 3) Info account 4) Consultare i movimenti 5) Log out";
+      return username+": si è evoluto in UtenteSaldoPassivo \n                                    Le operazioni permesse sono: 1) Consultare chi siamo 2) Richiedere prestito 3) Info account 4) Consultare i movimenti 5) Log out";
     case 3:
-      return username+": evoluzione in dashboard Admin \n                                    Le operazioni permesse sono: 1) Consultare la lista dei utenti registrati 2) Crea/Cancellare conti correnti 3) Cancellare account utenti 4) Info account 5) Log out";
+      return username+": si è evoluto in Admin \n                                    Le operazioni permesse sono: 1) Consultare la lista dei utenti registrati 2) Crea/Cancellare conti correnti 3) Cancellare account utenti 4) Info account 5) Log out";
   }
 }
 
