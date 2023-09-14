5c09ad745824aa8f1ee0ed58013a4882000da3e7
draibinadnan@gmail.com <draibinadnan@gmail.com>
2023-09-14 21:29:34 +0200

    fix ccAttivoSenior

--- be/classiBase/contoCorrenteAttivoSenior.js
+++ be/classiBase/contoCorrenteAttivoSenior.js
@@ -8,30 +8,7 @@ class ContoCorrenteAttivoSenior extends ContoCorrenteAttivoJunior{
       super(idContoCorrente);
       //this.idContoCorrente=idContoCorrente;
     }
-
-    /**
-     * Questa funzione si occupa di incrementare il saldo del conto corrente del beneficiario
-     * 
-     * @param {*} importo da versare 
-     * @returns msg
-    */
-    async versamento(importo){
-        this.saldo=await this.getSaldo();
-  
-        let sum=this.saldo+parseFloat(importo);
-  
-        return new Promise(resolve =>{
-            sql.query("UPDATE ContoCorrente SET saldo = ? WHERE idContoCorrente = ?;", [parseFloat(sum).toFixed(2), idContoCorrente],(err, data) => {
-                if (err) {
-                    console.log("error: ", err);
-                    resolve({message:err.message});
-                }
-  
-                resolve("ok");
-            })  
-        });
-    }
-
+    
     /**
      * Questa funzione si occupa di effettuare una ricarica telefonica
      * 
